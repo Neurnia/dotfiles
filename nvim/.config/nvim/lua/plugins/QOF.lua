9d4f08769da8da15fc5fcaf2529209056244cb23
Neurnia <leimengan@gmail.com>
2025-08-17 19:51:47 +0800

    fix(nvim): resolve install problem of markdown preview

--- nvim/.config/nvim/lua/plugins/QOF.lua
+++ nvim/.config/nvim/lua/plugins/QOF.lua
@@ -113,7 +113,8 @@ return {
 		pin = true,
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
-		build = "cd app && npm install",
+		-- install with npm
+		build = "cd app && npm install && git restore .",
 		init = function()
 			vim.g.mkdp_filetypes = { "markdown" }
 		end,
